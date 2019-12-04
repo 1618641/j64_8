b4d5b36ddc8a7a95c4b06a3bbe4351450cf485b8
1618641 <1618641@qq.com>
2019-12-04 17:38:13 +0800

    第一次提交

---
+++ src/org/java/demo/Demo.java
@@ -0,0 +1,12 @@
+package org.java.demo;
+
+/**
+ * @Author: 马果
+ * @Date: 2019/12/4 17:30
+ * @Description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("1111111111111");
+    }
+}
